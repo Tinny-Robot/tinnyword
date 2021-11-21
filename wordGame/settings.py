3e7f9838b2066a7ad5a3ac5dd3ddf3b9d6469fba
unknown <handanfoungmail.com>
2021-11-21 03:07:13 +0100

    make it bet m

--- wordGame/settings.py
+++ wordGame/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.43.62', 'tinnyword.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','tinnyword.herokuapp.com']
 
 
 # Application definition
